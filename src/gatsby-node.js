ae7e6a853d35d188f232ddce32ed8e51effb764f
Muhammad Muhajir <muhajirframe@gmail.com>
2018-10-05 15:16:55 +0700

    initial commit

---
+++ src/gatsby-node.js
@@ -0,0 +1,11 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+exports.onCreateBabelConfig = ({ actions: { setBabelPlugin } }) => {
+  setBabelPlugin({ name: 'babel-plugin-tailwind-components' })
+}
